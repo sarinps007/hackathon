6c9fc23c56304a023f615ef7c00c47fd59dd29eb
sarinps007 <70061082+sarinps007@users.noreply.github.com>
2020-08-22 23:11:13 +0530

    Update read.py
    changes made

--- read.py
+++ read.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 column=[]
-n = int(input('Enter number of colums needed'))
+n = int(input('Enter number of colums needed : '))
 for i in range(n):
     print(i+1,' column name : ')
     col=input()
     column.append(col);
-v= int(input('Enter number of rows needed')) 
+v= int(input('Enter number of rows needed : ')) 
 
 df=pd.read_csv("test.csv",usecols=column)
 final=df.head(v)
